4387902bb53856c86b0a5d1def4804e6a1429613
Andrada <117421285+ndrada@users.noreply.github.com>
2025-06-24 16:24:03 -0700

    fix: VS Code extension runs and kitty appears in custom activity bar view!

--- purrgrammer/extension.js
+++ purrgrammer/extension.js
@@ -1,21 +1,60 @@
-const vscode = require('vscode');
+const vscode = require('vscode'); //import vscode api
 
+//activate func runs when extension activates (when vscode loads it)
 function activate(context) {
-  const kittyStatus = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-  kittyStatus.text = '🐱 Sleeping...';
-  kittyStatus.tooltip = 'Click to feed kitty!';
-  kittyStatus.command = 'purrgrammer.feedKitty';
-  kittyStatus.show();
-
-  context.subscriptions.push(kittyStatus);
-
-  let feedKittyCommand = vscode.commands.registerCommand('purrgrammer.feedKitty', () => {
-    kittyStatus.text = '🐱 Mmm... yum!';
-    setTimeout(() => {
-      kittyStatus.text = '🐱 Sleeping...';
-    }, 4000);
-  });
-
-  context.subscriptions.push(feedKittyCommand);
+  context.subscriptions.push( //everything added here will be cleaned up when vscode disables the extension
+    vscode.window.registerWebviewViewProvider(
+      'purrgrammerPanel', //matches id used in package.json "views"
+      new KittyViewProvider(context.extensionUri) //creates a new panel view provider with the class below
+    )
+  ); 
 }
+
+class KittyViewProvider{
+  constructor(extensionUri){
+    this.extensionUri = extensionUri; //root folder needed later to load local files
+  }
+
+  resolveWebviewView(webviewView){
+    webviewView.webview.options = {
+      enableScripts: true, //let html panel run js - for interactivity
+      localResourceRoots: [vscode.Uri.joinPath(this.extensionUri, 'media')]
+    };
+    //store html content for webview panel
+    webviewView.webview.html = this.getHtml(webviewView.webview);
+  }
+
+  //actual html panel that will be displayed
+  getHtml(webview){
+    //special uri to safely access gifs
+    const catGif = webview.asWebviewUri(
+      vscode.Uri.joinPath(this.extensionUri, 'media', 'awesome-cat.gif')
+    );
+
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          /*centering everything with a nice dark background*/
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; 
+            background: #191919;
+          }
+          img { width: 120px; } /*image size*/
+        </style>
+      </head>
+      <body>
+        <img src="${catGif}" alt="kitty gif" />
+      </body>
+    </html>
+    `;
+  }
+}
+
 exports.activate = activate;
